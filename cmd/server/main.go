11ad9c1a608c18379257b4b0855e5a158cdf9c9d
akmalsulaymonov <akmalsulaymonov@gmail.com>
2023-03-30 01:06:49 +0500

    Connecting to Database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/akmalsulaymonov/go-rest-api/internal/db"
+)
 
 // Run - is going to be responsible for the instantiation and startup
 // of our go application
 func Run() error {
 	fmt.Println("Starting up our application")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to the database")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("Successfully connected and pinged database")
+
 	return nil
 }
 
